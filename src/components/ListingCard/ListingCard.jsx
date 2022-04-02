04e9e71e0cf69f8c10b6e56ef07d2ab9ebeda792
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-04-02 23:45:29 +0530

    add: rating star to cards

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./ListingCard.css";
-import ForzaImg from "../../assets/listing-images/forza-horizon.png";
 import { BsHeart } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
 const ListingCard = ({ productItem }) => {
-  const { title, price, imgURL } = productItem;
+  const { title, price, imgURL, rating } = productItem;
   return (
     <div className="listing-card">
       <div className="listing-img-container">
@@ -15,6 +15,10 @@ const ListingCard = ({ productItem }) => {
         <div className="listing-card-details">
           <p className="listing-card-name">{title}</p>
           <p className="listing-card-price">₹{price}.00</p>
+          <p className="listing-card-rating">
+            <AiFillStar />
+            {rating}
+          </p>
         </div>
         <div className="listing-card-actions">
           <button className="btn card-action-btn">ADD TO CART</button>
--- src/helpers/filterHelpers/ratingFilter.js
+++ src/helpers/filterHelpers/ratingFilter.js
@@ -1,6 +1,6 @@
 export const applyRatingFilter = (products, rating) => {
   const filteredArray = products.filter((product) => {
-    Math.floor(product.rating) >= rating;
+    return Math.floor(product.rating) >= rating;
   });
   return filteredArray;
 };
